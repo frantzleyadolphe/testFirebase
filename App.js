d9fcf56e05fce432344c77274dc1367cafc34e13
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-09-30 01:30:06 -0400

    code for screen content added

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreens} />
+      <Stack.Screen name="Login" options={{ headerShown: false }} component={LoginScreens} />
       <Stack.Screen name="Home" component={HomeScreens} />
     </Stack.Navigator>
   </NavigationContainer>
--- src/Screens/Login/LoginScreens.jsx
+++ src/Screens/Login/LoginScreens.jsx
@@ -1,12 +1,78 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginScreens = () => {
+  const [email, setEmail]=React.useState('');
+  const [password, setPassword]=React.useState('');
   return (
-    <View>
-      <Text>LoginScreens</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "gray" }}>
+      <View style={login.container}>
+        <View style={login.inputContainer}>
+          <TextInput style={login.input} 
+          placeholder="Email" 
+          value={email}
+          onChangeText={(text)=>setEmail(text)}
+          />
+          <TextInput style={login.input} 
+          placeholder="Email" 
+          value={password}
+          onChangeText={(text)=>setPassword(text)}
+          secureTextEntry
+          />
+        </View>
+        <TouchableOpacity style={login.btn}>
+          <Text>
+            LOGIN
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={login.btn2}>
+          <Text style={{ color: "white",}}>
+            SIGN IN
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default LoginScreens
+export default LoginScreens;
+
+const login = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  btn:{
+    backgroundColor: "yellow",
+    width: "50%",
+    alignItems: "center",
+    borderRadius: "5px",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  btn2:{
+    backgroundColor: "green",
+    width: "50%",
+    alignItems: "center",
+    borderRadius: "5px",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  }
+};
