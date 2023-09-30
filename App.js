63404e226ac504bb9991e870d1479bc1bbc9883a
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-09-30 00:42:54 -0400

    route for navigator added

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreens from './src/Screens/Login/LoginScreens';
+import HomeScreens from './src/Screens/Home/HomeScreens';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreens} />
+      <Stack.Screen name="Home" component={HomeScreens} />
     </Stack.Navigator>
   </NavigationContainer>
   );
