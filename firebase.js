2f19a83d9636e7b15643ae69bdf12e0831706919
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-09-30 01:30:53 -0400

    firebase.js file added with credentials

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo1Cxp5k3_cqDxQpk13HNp-pOZZOeesIA",
+  authDomain: "fir-auth-58ca9.firebaseapp.com",
+  projectId: "fir-auth-58ca9",
+  storageBucket: "fir-auth-58ca9.appspot.com",
+  messagingSenderId: "720959427765",
+  appId: "1:720959427765:web:633d2fdfe6d74f1544dafc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
